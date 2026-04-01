60fa53e80611df8457f961a18889d3338f3bdd7e
muzafferyn <relaxofficialz@icloud.com>
2026-04-01 19:24:36 +0300

    qweqweew

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
     output: 'export',
     basePath: '/cvv',
     assetPrefix: '/cvv/',
 }
+
+module.exports = nextConfig
